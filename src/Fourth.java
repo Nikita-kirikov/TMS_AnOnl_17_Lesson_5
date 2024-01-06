227429a816d2f14a00e4b3a3cedd52d66121956f
Nikita Kirikov <n.kiirkov@gmail.com>
2024-01-06 18:07:35 +0300

    fix fourth task

--- src/Fourth.java
+++ src/Fourth.java
@@ -1,15 +1,15 @@
 public class Fourth {
     public static void Run() {
         int[][] numbers = {
-                {4,3,2},
+                {4,3,2,5},
                 {1,2,3,4},
-                {4,3},
+                {4,3,6,3},
                 {1,2,3,4},
-                {2,5,4,3,2,1} };
+                {2,5,4,3} };
         int index = 0;
 
-        for (int i = 0; i < numbers.length; i++) {
-            if(index < numbers.length) {
+        for (int[] number : numbers) {
+            if (index < number.length) {
                 System.out.println(numbers[index][index]);
                 index++;
             }
